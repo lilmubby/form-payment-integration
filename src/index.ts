ad0fddce30a1416a97c5adff5f5b537246f0a77e
Mubarak Yahaya <ymubarak247@gmail.com>
2025-08-03 01:14:04 +0100

    fix: build fail due to module type

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
-require("dotenv").config();
-
+import dotenv from 'dotenv';
 import  application from "./app.js"
 import { config } from "./config/env.js";
 
+dotenv.config();
 
 async function startServer() {
   const PORT = config.PORT || 5000;
