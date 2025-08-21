8bdfdc63dc2a16f9825820821878768e74b3e7c9
Mubarak Yahaya <ymubarak247@gmail.com>
2025-08-21 15:16:26 +0100

    feat: randome commit

--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,7 @@ async function startServer() {
   const PORT = config.PORT;
 
   application.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+    console.log(`Server## is running on port ${PORT}`);
   });
 }
 
